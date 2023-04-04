f520bb6e28eb9d25751180571cbf2c8e70298a97
Wesam Nabeel <wesamezio@gmail.com>
2023-04-04 06:11:55 +0300

    implement model

--- neural_network/neural_network.ino
+++ neural_network/neural_network.ino
@@ -6,9 +6,21 @@ const int IMAGE_SIZE = 28;
 const int N_INPUT = IMAGE_SIZE * IMAGE_SIZE;  // square image size
 const int N_OUTPUT = 10;      // 10 classes (digits 0-9)
 const int KERNEL_SIZE = 3;
-const int N_KERNAL = 3; // kernal size
+const int N_KERNAL = 3; 
+const int STRIDE = 2;
+const int CONVOLVED_IMAGE_SIZE = IMAGE_SIZE-KERNEL_SIZE+1;
+const int POOL_SIZE = CONVOLVED_IMAGE_SIZE/STRIDE;
 
+int input_image[IMAGE_SIZE][IMAGE_SIZE];
 
+float kernel[KERNEL_SIZE][KERNEL_SIZE];
+float convolved_image[CONVOLVED_IMAGE_SIZE][CONVOLVED_IMAGE_SIZE];
+float pool_output[POOL_SIZE][POOL_SIZE];
+
+float output_weights[N_INPUT][N_OUTPUT];
+float output[N_OUTPUT];
+
+  
 // sigmoid activation function
 double sigmoid(double x) {
   return 1.0 / (1.0 + exp(-x));
@@ -17,20 +29,17 @@ double sigmoid(double x) {
 
 void setup() {
   Serial.begin(9600);
+
+  Serial.println("setup done  !!");
+  
 }
 
 void loop() {
-
-  // init variables
-  float input_image[IMAGE_SIZE][IMAGE_SIZE];
   
-  float kernel[KERNEL_SIZE][KERNEL_SIZE];
-  int convolved_image_size = N_INPUT-KERNEL_SIZE+1;
-
-  float convolved_image[convolved_image_size][convolved_image_size];
-  
-  for (int i=0;i<convolved_image_size;i++) {
-    for (int j=0;j<convolved_image_size;j++) {
+  // convolve the input image
+    Serial.println("convolution!!!");
+  for (int i=0;i<CONVOLVED_IMAGE_SIZE;i++) {
+    for (int j=0;j<CONVOLVED_IMAGE_SIZE;j++) {
       float sum = 0;
       for (int k=0;k<KERNEL_SIZE;k++) {
         for (int l=0;l<KERNEL_SIZE;l++) {
@@ -40,9 +49,45 @@ void loop() {
       convolved_image[i][j] = sum;
       }
   }
-  
-  float output_weights[N_INPUT][N_OUTPUT];
-  float output[N_OUTPUT];
+  Serial.println("max pooling!");
+  // implement max pooling
+    for (int i = 0; i < CONVOLVED_IMAGE_SIZE; i += STRIDE) {
+        for (int j = 0; j < CONVOLVED_IMAGE_SIZE; j += STRIDE) {
+            int max_val = convolved_image[i][j];
+            for (int m = i; m < i + STRIDE; m++) {
+                for (int n = j; n < j + STRIDE; n++) {
+                    if (convolved_image[m][n] > max_val) {
+                        max_val = convolved_image[m][n];
+                    }
+                }
+            }
+            pool_output[i/STRIDE][j/STRIDE] = max_val;
+        }
+    }
+
+    
+  // Reshape the 2D array to a 1D array for input to the neural network
+  float flattened_input[POOL_SIZE* POOL_SIZE];
+  int k = 0;
+  for (int i = 0; i < POOL_SIZE; i++) {
+    for (int j = 0; j < POOL_SIZE; j++) {
+      flattened_input[k] = pool_output[i][j];
+      k++;
+    }
+  }
+
+  // Feed the flattened input to the fully connected neural network
+  for (int i = 0; i < N_OUTPUT; i++) {
+    float sum = 0;
+    for (int j = 0; j < N_INPUT; j++) {
+      sum += flattened_input[j] * output_weights[j][i];
+    }
+    output[i] = sigmoid(sum);
+  }
+
+      Serial.println("output!!!");
+
+
 
 
 }
